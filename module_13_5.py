1dea98a661c476ff6e2e49c169f8607188f94274
TheMsSalina <salinar@inbox.ru>
2024-12-20 23:46:19 +0300

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = '8186664377:AAGbBEOd_FOptpmTm8g-tIAGEdhZp_fuX0M'
+api = ''
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
